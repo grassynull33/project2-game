61fc3770241117b625ba299f7f702dcb00f130ea
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-10 23:06:44 -0700

    basic server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var path = require('path');
+var routes = require('./routes/index.js');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var db = require('./models');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    console.log('Listening on PORT ' + PORT);
+  });
+});
