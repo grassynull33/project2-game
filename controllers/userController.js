b73f63822f8e68454ccb5816e1e6f10717ec003d
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-10 23:24:58 -0700

    save email to mysql database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
-var User = require('../models/User.js');
+var db = require('../models');
 
 exports.registerForm = function (req, res) {
   res.render('index');
+};
+
+exports.register = function (req, res) {
+  console.log(req.body);
+  db.User.create({
+    email: req.body.email,
+    password: req.body.password
+  }).then(function () {
+    res.send('WHOA');
+  });
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var userController = require('../controllers/userController.js');
 
 router.get('/register', userController.registerForm);
+router.post('/register', userController.register);
 
 module.exports = router;
