fc844e3c70e83d1d617f7e1e8332ea96134ddbe0
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-13 13:37:39 -0700

    connect firebase

--- bin/www.js
+++ bin/www.js
@@ -11,18 +11,18 @@ var debug = require('debug')('express-example');
 var app = require('../server');
 
 // we bring in the models we exported with index.js
-var models = require("../models");
+var models = require('../models');
 
 // we set the port of the app
 app.set('port', process.env.PORT || 3000);
 
-
-// we sync the models with our db 
+// we sync the models with our db
 // (thus creating the apropos tables)
 models.sequelize.sync().then(function () {
-	// set our app to listen to the port we set above
-  var server = app.listen(app.get('port'), function() {
-  	// then save a log of the listening to our debugger.
+  // set our app to listen to the port we set above
+  var server = app.listen(app.get('port'), function () {
+    // then save a log of the listening to our debugger.
     debug('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on port ' + server.address().port);
   });
 });
---
+++ public/assets/javascript/app.js
@@ -0,0 +1,12 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyDDgV4ff6fwgBOA-aj7au6rU_a2ov4Etcw',
+    authDomain: 'project2-e15c9.firebaseapp.com',
+    databaseURL: 'https://project2-e15c9.firebaseio.com',
+    projectId: 'project2-e15c9',
+    storageBucket: 'project2-e15c9.appspot.com',
+    messagingSenderId: '350780582751'
+  };
+  firebase.initializeApp(config);
+
+  console.log('APP JS');
