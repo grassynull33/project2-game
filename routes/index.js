7baf4bada948a463a71ee64a698228a60816184e
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-17 11:35:31 -0700

    modularization of achievements code into separate controller

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-
 var firebase = require('firebase-admin');
 var express = require('express');
 var router = express.Router();
+var achievementController = require('../controllers/achievementController');
 
 var db = require('../models');
 
@@ -42,75 +42,7 @@ router.get('/firebase', function (req, res) {
 
 router.get('/', function (req, res) {
   console.log('GET REQUEST IN INDEX.jS (ROUTES)');
-
-  var achievements = {
-    hoarder: false,
-    needle: false,
-    quantity: false,
-    collection: true,
-    ultimate: false
-  };
-
-  var data = {
-    items: []
-  };
-
-  var collectedAll = true;
-
-  var allItems = [
-    'Cube of Destruction',
-    'Archon_Helm',
-    'Icelandic Glacial Spikes',
-    'Popa Pola',
-    'W00T'
-    // fill in all possible items
-  ];
-
-  // collection of related items achievement
-  var collectionItems = [
-    'Baseball',
-    'Bat',
-    'Catcher\'s Mitt'
-  ];
-
-  db.Item.findAll().then(function (results) {
-    for (var i = 0; i < results.length; i++) {
-      data.items.push(results[i].dataValues);
-      if (allItems.indexOf(results[i].dataValues.name) === -1) {
-        collectedAll = false;
-      }
-
-      if (collectionItems.indexOf(results[i].dataValues.name) === -1) {
-        achievements.collection = false;
-      }
-
-      // hoarder
-      if (results.length >= 40) {
-        achievements.hoarder = true;
-      }
-
-      // needle in a haystack
-      if (results[i].dataValues.name === 'Super Rare Item') {
-        achievements.needle = true;
-      }
-
-      db.Item.count({ where: {'name': results[i].dataValues.name}}).then(function (count) {
-        console.log('there are ' + count);
-        if (count > 4) {
-          achievements.quantity = true;
-        }
-      });
-    }
-  }).then(function () {
-    // ultimate collector
-    if (collectedAll) {
-      achievements.ultimate = true;
-    }
-
-    // console.log(data);
-    console.log(achievements);
-    res.render('index', achievements);
-  });
+  achievementController.checkAchievements;
 });
 
 module.exports = router;
