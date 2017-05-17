af2d5cd88b916287cc51e0300518d3f40c9c118a
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-17 02:26:26 -0700

    achievements section of web app, minor styling for achievements, incomplete route index.js (async)

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,75 @@ router.get('/firebase', function (req, res) {
 
 router.get('/', function (req, res) {
   console.log('GET REQUEST IN INDEX.jS (ROUTES)');
-  res.render('index');
+
+  var achievements = {
+    hoarder: false,
+    needle: false,
+    quantity: false,
+    collection: true,
+    ultimate: false
+  };
+
+  var data = {
+    items: []
+  };
+
+  var collectedAll = true;
+
+  var allItems = [
+    'Cube of Destruction',
+    'Archon_Helm',
+    'Icelandic Glacial Spikes',
+    'Popa Pola',
+    'W00T'
+    // fill in all possible items
+  ];
+
+  // collection of related items achievement
+  var collectionItems = [
+    'Baseball',
+    'Bat',
+    'Catcher\'s Mitt'
+  ];
+
+  db.Item.findAll().then(function (results) {
+    for (var i = 0; i < results.length; i++) {
+      data.items.push(results[i].dataValues);
+      if (allItems.indexOf(results[i].dataValues.name) === -1) {
+        collectedAll = false;
+      }
+
+      if (collectionItems.indexOf(results[i].dataValues.name) === -1) {
+        achievements.collection = false;
+      }
+
+      // hoarder
+      if (results.length >= 40) {
+        achievements.hoarder = true;
+      }
+
+      // needle in a haystack
+      if (results[i].dataValues.name === 'Super Rare Item') {
+        achievements.needle = true;
+      }
+
+      db.Item.count({ where: {'name': results[i].dataValues.name}}).then(function (count) {
+        console.log('there are ' + count);
+        if (count > 4) {
+          achievements.quantity = true;
+        }
+      });
+    }
+  }).then(function () {
+    // ultimate collector
+    if (collectedAll) {
+      achievements.ultimate = true;
+    }
+
+    // console.log(data);
+    console.log(achievements);
+    res.render('index', achievements);
+  });
 });
 
 module.exports = router;
