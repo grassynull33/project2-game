88a94c23022274ed84fc82d6a8b130119829a462
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-18 11:49:57 -0700

    tweak index.js routes for login after merge with yen

--- routes/index.js
+++ routes/index.js
@@ -51,12 +51,13 @@ router.get('/',
 );
 
 // login
-router.post('/login', passport.authenticate('local'), function (req, res) {
+router.post('/users/login', passport.authenticate('local'), function (req, res) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
   // res.json('/');
-  res.send({redirect: '/'});
+  // res.send({redirect: '/'});
+  res.json('/');
 });
 
 // Route for getting some data about our user to be used client side
