2093688bc04938e72d56c55fc4543166e4e32d75
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-18 12:12:09 -0700

    fix user auth issues with routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,12 +10,12 @@ var isAuthenticated = require('../config/middleware/isAuthenticated');
 //   });
 // });
 
-// this is the users_controller.js file
-router.get('/signup', function (req, res) {
-  res.render('registration', {
-    layout: 'main-registration'
-  });
-});
+// // this is the users_controller.js file
+// router.get('/signup', function (req, res) {
+//   res.render('registration', {
+//     layout: 'main-registration'
+//   });
+// });
 
 router.get('/sign-out', function (req, res) {
   req.logout();
@@ -30,30 +30,30 @@ router.get('/sign-out', function (req, res) {
 //   res.json('/');
 // });
 
-// register a user
-router.post('/signup', function (req, res) {
-  db.User.findAll({
-    where: {username: req.body.username}
-  }).then(function (users) {
-    if (users.length > 0) {
-      res.json({
-        duplicateUser: true
-      });
-    // At some point, make sure that only one user can be associated with an email.
-    } else {
-      db.User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-      }).then(function () {
-        // res.send({redirect: '/'});
-        res.redirect(307, '/login');
-        // res.redirect('/');
-      }).catch(function (err) {
-        res.json(err);
-      });
-    }
-  });
-});
+// // register a user
+// router.post('/signup', function (req, res) {
+//   db.User.findAll({
+//     where: {username: req.body.username}
+//   }).then(function (users) {
+//     if (users.length > 0) {
+//       res.json({
+//         duplicateUser: true
+//       });
+//     // At some point, make sure that only one user can be associated with an email.
+//     } else {
+//       db.User.create({
+//         username: req.body.username,
+//         email: req.body.email,
+//         password: req.body.password
+//       }).then(function () {
+//         // res.send({redirect: '/'});
+//         res.redirect(307, '/login');
+//         // res.redirect('/');
+//       }).catch(function (err) {
+//         res.json(err);
+//       });
+//     }
+//   });
+// });
 
 module.exports = router;
