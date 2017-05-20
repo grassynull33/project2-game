83f35dd6e63938b5048e7c594e91976573756c8a
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-19 23:04:38 -0700

    fb cred pt 2

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ var serviceAccount = {
   'private_key_id': process.env.PRIVATE_KEY_ID,
   'private_key': process.env.PRIVATE_KEY,
   'client_email': process.env.CLIENT_EMAIL,
-  'client_id': process.env.CLIENT_EMAIL,
+  'client_id': process.env.CLIENT_ID,
   'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
   'token_uri': 'https://accounts.google.com/o/oauth2/token',
   'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
